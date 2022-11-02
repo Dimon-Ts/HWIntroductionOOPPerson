c7609867058ff6b2afee3661b8b2699364c9e352
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-11-02 22:03:52 +0300

    ООП. Урок 1 (введение, object),домашнее задание 1,2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        Person maxim = new Person("Максим","Минск",1988,"бренд-менеджер");
+        maxim.greeting();
+        Person anya = new Person("Аня","Москва",1993,"методист образовательных программ");
+        anya.greeting();
+        Person katia = new Person("Катя","Калининград",1992,"продакт-менеджер");
+        katia.greeting();
+        Person artem = new Person("Артём","Москва",1995,"директор по развитию бизнеса");
+        artem.greeting();
     }
 }
